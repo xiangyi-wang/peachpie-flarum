5d9deaf9c7f2f8cb92ce5bd0c1d90d8c9b835582
xiangyi wang <2206037943@qq.com>
2023-06-11 11:46:32 +0800

    use Peachpie Templates

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,83 +1,48 @@
-using System;
-using System.IO;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Hosting;
 
-namespace Flarum.Server
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//builder.Services.AddRazorPages();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var host = WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://*:5004/")
-                .Build();
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+});
+builder.Services.AddPhp();
 
-            host.Run();
-        }
+builder.WebHost.UseUrls("https://*:9001");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+    }
+var contentPath = ResolveContentPath();
+
+
+
+app.UseDefaultFiles();
+app.UseStaticFiles(new StaticFileOptions { FileProvider = new PhysicalFileProvider(contentPath) });
+app.UseRouting();
+app.UsePhp("/../", rootPath: contentPath);
+//app.UseAuthorization();
+
+//app.MapRazorPages();
+static string ResolveContentPath()
+{
+    var devcontent = Path.GetFullPath("/../flarum");
+    if (Directory.Exists(devcontent))
+    {
+        return devcontent;
     }
 
-    class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Adds a default in-memory implementation of IDistributedCache.
-            services.AddDistributedMemoryCache();
-
-            services.AddSession(options =>
-            {
-                options.IdleTimeout = TimeSpan.FromMinutes(30);
-                options.Cookie.HttpOnly = true;
-            });
-
-            services.AddPhp(options =>
-            {
-                //
-            });
-        }
-
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
-        {
-            //// sample usage of URL rewrite:
-            //var options = new RewriteOptions()
-            //    .AddRewrite(@"^rule/(\w+)", "index.php?word=$1", skipRemainingRules: true);
-
-            //app.UseRewriter(options);
-
-            // enable session:
-            app.UseSession();
-
-            // enable .php files from compiled assembly:
-            var contentPath = ResolveContentPath();
-
-            app.UsePhp("/", rootPath: contentPath);
-            app.UseStaticFiles(new StaticFileOptions { FileProvider = new PhysicalFileProvider(contentPath) });
-            
-            //
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-        }
-
-        /// <summary>
-        /// Gets location of website project content.
-        /// In development, we use the original website project location.
-        /// Otherwise, content files are published to the current working directory.
-        /// </summary>
-        /// <returns></returns>
-        static string ResolveContentPath()
-        {
-            var devcontent = Path.GetFullPath("../Website");
-            if (Directory.Exists(devcontent))
-            {
-                return devcontent;
-            }
-
-            return Directory.GetCurrentDirectory();
-        }
-    }
+    return Directory.GetCurrentDirectory();
 }
+
+app.Run();
